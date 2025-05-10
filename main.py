31040e9d5742a43d45a794d2be1bd7eec9ae97fc
Chinyiskan <david.camilo.charry@gmail.com>
2025-05-10 16:47:10 -0500

    Add greeting functionality with a customizable name

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Howdy, partner!")
+
+def greet(name):
+    """
+    Greets the user with their name.
+    
+    Args:
+        name (str): The name of the user.
+    
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
